2779f84be9258ffc2baee6cd5fec7512bffeaa6d
davide-belli <davidebelli95@gmail.com>
2025-04-13 19:49:51 +0200

    Added utils to contest templates

--- templates/contest/f/f.py
+++ templates/contest/f/f.py
@@ -3,6 +3,10 @@ import sys
 import os
 from io import BytesIO, IOBase
 
+from math import gcd, floor, sqrt, log, ceil
+from collections import defaultdict, Counter, deque
+from bisect import bisect_left, bisect_right, insort_left, insort_right
+
 BUFSIZE = 8192
 
 
@@ -53,6 +57,41 @@ class IOWrapper(IOBase):
 
 sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
 input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+
+def prime_factors(n):
+    i = 2
+    primfac = []
+    while i * i <= n:
+        while n % i == 0:
+            primfac.append(i)
+            n = n / i
+        i = i + 1
+    if n > 1:
+        primfac.append(int(n))
+    return primfac
+
+def prime_sieve(n):
+    is_prime = [True for _ in range(n + 1)]
+    is_prime[0] = is_prime[1] = False
+    i = 2
+    while i * i <= n:
+        if is_prime[i]:
+            j = i * i
+            while j <= n:
+                is_prime[j] = False
+                j += i
+        i += 1
+    return is_prime
+
+def find_divisors(number):
+    divisors = []
+    for i in range(1, int(number**0.5) + 1):
+        if number % i == 0:
+            divisors.append(i)
+            if i != number // i:
+                divisors.append(number // i)
+    return divisors
 
 
 def in_int():
